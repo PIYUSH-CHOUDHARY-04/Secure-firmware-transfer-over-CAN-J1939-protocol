df33d874b15858a18d622f72ea15b4ecd53f03cd
Piyush Choudhary <mercmerc961@gmail.com>
2024-12-06 19:03:36 +0530

    Delete Project  J1939/Code and Implementation directory

--- Project  J1939/Code and Implementation/Debugger programming/debugger0/debugger0.c
+++
@@ -1,8 +0,0 @@
-#define GPIO_PTR GPIOA
-static void Display(uint8_t num){
-
-	HAL_GPIO_WritePin(GPIO_PTR, num, GPIO_PIN_SET);
-	HAL_Delay(5000);
-	HAL_GPIO_WritePin(GPIO_PTR, num, GPIO_PIN_RESET);
-	HAL_Delay(5000);
-}
--- Project  J1939/Code and Implementation/Programmer Node programming/Firmware/format conversion/hex2bin.c
+++
@@ -1,3 +0,0 @@
-#include "hex2bin.h"
-
-
